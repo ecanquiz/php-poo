03c437fdbd9e6f9c03e19a41c390dd636efe79ad
Ernesto Canquiz <fdsoil123@gmail.com>
2023-06-09 19:59:41 -0400

    deploy

---
+++ assets/intro.md.57d39b5e.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as a,O as s}from"./chunks/framework.a6ecc25e.js";const h=JSON.parse('{"title":"Introducción","description":"","frontmatter":{},"headers":[],"relativePath":"intro.md"}'),r={name:"intro.md"},t=s("",19),n=[t];function i(l,d,u,c,m,p){return a(),o("div",null,n)}const b=e(r,[["render",i]]);export{h as __pageData,b as default};
--- assets/intro.md.a7b5b21f.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as o,o as a,O as s}from"./chunks/framework.a6ecc25e.js";const h=JSON.parse('{"title":"Introducción","description":"","frontmatter":{},"headers":[],"relativePath":"intro.md"}'),r={name:"intro.md"},t=s("",22),n=[t];function i(l,d,u,c,p,m){return a(),o("div",null,n)}const b=e(r,[["render",i]]);export{h as __pageData,b as default};
